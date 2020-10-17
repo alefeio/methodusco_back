cf0c3d043623363e4a7737a131f4e8589bfd6141
Alexandre Feio <alefeio@gmail.com>
2020-10-17 01:38:23 -0300

    alt2

--- src/app.js
+++ src/app.js
@@ -1,24 +1,51 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import io from 'socket.io';
+import http from 'http';
 
 import './database';
 
 class App {
   constructor() {
-    this.server = express();
+    this.app = express();
+    this.server = http.Server(this.app);
+
+    this.socket();
 
     this.middlewares();
     this.routes();
+
+    this.connectedUsers = {};
+  }
+
+  socket() {
+    this.io = io(this.server);
+
+    this.io.on('connection', (socket) => {
+      const { usuario_id } = socket.handshake.query;
+      this.connectedUsers[usuario_id] = socket.id;
+
+      socket.on('disconnect', () => {
+        delete this.connectedUsers[usuario_id];
+      });
+    });
   }
 
   middlewares() {
-    this.server.use(cors());
-    this.server.use(express.json());
+    this.app.use(cors());
+    this.app.use(express.json());
+
+    this.app.use((req, res, next) => {
+      req.io = this.io;
+      req.connectedUsers = this.connectedUsers;
+
+      next();
+    })
   }
 
   routes() {
-    this.server.use(routes);
+    this.app.use(routes);
   }
 }
 
