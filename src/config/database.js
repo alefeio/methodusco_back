883e5a7316f88ef62a7292aa831fdab0b6beea0d
Alexandre Feio <alefeio@gmail.com>
2020-10-17 00:53:26 -0300

    alteracoes no database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,6 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  // host: '192.168.99.100',
-  // // host: 'localhost',
-  // username: 'postgres',
-  // password: '301159',
-  // database: 'methodusco',
-  // database: 'methodusldm',
   host: process.env.BD_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
