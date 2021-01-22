618d66207a745d8f0aaa9e2fbb8db2036353f8e2
Alexandre Feio <alefeio@gmail.com>
2021-01-22 15:01:42 -0300

    alt3

--- src/database/migrations/20210122171523-add-field-aula-provas2s.js
+++ src/database/migrations/20210122171523-add-field-aula-provas2s.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('aula', 'provas2s', {
+    return queryInterface.addColumn('provas2s', 'aula', {
       type: Sequelize.INTEGER,
       defaultValue: 0,
       allowNull: false,
@@ -10,6 +10,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    return queryInterface.removeColumn('aula', 'provas2s');
+    return queryInterface.removeColumn('provas2s', 'aula');
   },
 };
