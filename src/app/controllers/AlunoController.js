4fe7f48c4f8c9d01489ed7fe054d69c5ec98b301
Alexandre Feio <alefeio@gmail.com>
2021-03-25 12:29:30 -0300

    alteracao na ordem de listagem de alunos - 2

--- src/app/controllers/AlunoController.js
+++ src/app/controllers/AlunoController.js
@@ -4,8 +4,8 @@ class AlunoController {
   async index(req, res) {
     const usuarios = await Usuario.findAll({
       where: { admin: false },
+      order: [['id', 'DESC']],
       attributes: ['id', 'nome', 'email', 'updated_at'],
-      order: [['updated_at', 'desc']],
     });
 
     return res.json(usuarios);
