71ffd94340023d7c48a5a8209a9961857e107020
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2021-09-29 01:17:00 -0300

    correcaodeleteusuario4

--- src/app/controllers/AlunoController.js
+++ src/app/controllers/AlunoController.js
@@ -3,7 +3,7 @@ import Usuario from '../models/Usuario';
 class AlunoController {
   async index(req, res) {
     const usuarios = await Usuario.findAll({
-      where: { admin: false },
+      where: { admin: false, ativo: true },
       order: [['updated_at', 'DESC']],
       attributes: ['id', 'nome', 'email', 'updated_at'],
     });
