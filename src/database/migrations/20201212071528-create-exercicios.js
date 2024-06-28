e0d46e9e54fee6b31e171b7328d45f03099cbe9a
Alexandre Feio <alefeio@gmail.com>
2024-06-28 16:20:16 -0300

    remove migration resposta - ajust

--- src/database/migrations/20201212071528-create-exercicios.js
+++ src/database/migrations/20201212071528-create-exercicios.js
@@ -2,49 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('exercicios', {
+    return queryInterface.createTable('categoria', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
       },
-      questao: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      subquestao: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      resposta: {
-        type: Sequelize.INTEGER,
-      },
-      categoria_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'categoria', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: false,
-      },
-      modulo_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'modulos', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: false,
-      },
-      tipo_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'tipos', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      },
-      admin_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'usuarios', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
+      nome: {
+        type: Sequelize.STRING,
         allowNull: false,
       },
       created_at: {
@@ -59,6 +25,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('exercicios');
+    return queryInterface.dropTable('categoria');
   },
 };
