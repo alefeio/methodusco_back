be675e8d084bc5d260e92fea9932f87f7228364d
alefeio@gmail.com <alefeio@gmail.com>
2024-07-01 11:36:25 -0300

    db

--- bkp_db_methodusco_20-04-2021_02_37_18.sql
+++ bkp_db_methodusco_20-04-2021_02_37_18.sql
@@ -7,10 +7,6 @@ SET default_transaction_read_only = off;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 
---
--- Drop databases (except postgres and template1)
---
-
 DROP DATABASE methodusco;
 
 
@@ -39,9 +35,6 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 -- Databases
 --
 
---
--- Database "template1" dump
---
 
 --
 -- PostgreSQL database dump
@@ -61,19 +54,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-UPDATE pg_catalog.pg_database SET datistemplate = false WHERE datname = 'template1';
-DROP DATABASE template1;
---
--- Name: template1; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
-
-
-ALTER DATABASE template1 OWNER TO postgres;
-
-\connect template1
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -85,21 +65,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: DATABASE template1; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE template1 IS 'default template for new databases';
-
-
---
--- Name: template1; Type: DATABASE PROPERTIES; Schema: -; Owner: postgres
---
-
-ALTER DATABASE template1 IS_TEMPLATE = true;
-
-
-\connect template1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -111,13 +76,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Name: DATABASE template1; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE CONNECT,TEMPORARY ON DATABASE template1 FROM PUBLIC;
-GRANT CONNECT ON DATABASE template1 TO PUBLIC;
 
 
 --
